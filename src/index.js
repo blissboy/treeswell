4223a9d50e5dffed1f2fb1afb6a2984aea2d14d0
pat <jonpat@gmail.com>
2018-10-21 22:11:40 -0500

    just rearranging

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 "use strict";
+
+var midi = require('WebMidi');
+console.log('initializing midi');
+midi.enable( (err) => {
+    if ( err ) {
+        console.log("Could not midi", err);
+    } else {
+        console.log("enabled midi");
+        console.log('inputs' + midi.inputs);
+        console.log(`outputs: ${midi.outputs}`)
+
+    }
+});
+
 var THREE = require('THREE');
 var OrbitControls = require('three-orbit-controls')(THREE);
 var TreeModel = require('tree-model');
@@ -36,6 +50,10 @@ var framecount = 0;
 
 var tree;
 
+
+
+
+
 Number.prototype.map = function (in_min, in_max, out_min, out_max) {
     return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
@@ -54,48 +72,10 @@ function animate() {
 function newInit() {
     gui = new dat.GUI();
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000000);
-    camera.position.x = MAX_TREE_HEIGHT * .29;
-    camera.position.y = MAX_TREE_HEIGHT * .63;
-    camera.position.z = MAX_TREE_HEIGHT * .25;
 
-    camera.lookAt(0, MAX_TREE_HEIGHT / 2, MAX_TREE_HEIGHT);
-
-    renderer = new THREE.WebGLRenderer({antialias: true, preserveDrawingBuffer: true});
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor(0x000000, 1);
-    document.body.appendChild(renderer.domElement);
-
-    cameraControls = new OrbitControls(camera, renderer.domElement);
-    cameraControls.maxAzimuthAngle = Math.PI / 2;
-    cameraControls.maxPolarAngle = Math.PI / 2;
-    cameraControls.target = new THREE.Vector3(-2722, 32248, -4674);
-    cameraControls.update();
-
-    //orbit.enableZoom = false;
-
-    var lights = [];
-    lights[0] = new THREE.PointLight(0xffffff, 1, 0);
-    lights[1] = new THREE.PointLight(0xffffff, 1, 0);
-    lights[2] = new THREE.PointLight(0xffffff, 1, 0);
-    lights[3] = new THREE.PointLight(0xffffff, 1, 0);
-    lights[4] = new THREE.PointLight(0xffffff, 1, 0);
-    lights[5] = new THREE.PointLight(0xffffff, 1, 0);
-
-    lights[0].position.set(0, MAX_TREE_HEIGHT, 0);
-    lights[1].position.set(MAX_TREE_HEIGHT, 0, 0);
-    lights[2].position.set(0, 0, -1 * MAX_TREE_HEIGHT);
-    lights[3].position.set(0, -1 * MAX_TREE_HEIGHT, 0);
-    lights[4].position.set(-1 * MAX_TREE_HEIGHT, 0, 0);
-    lights[5].position.set(0, 0, -1 * MAX_TREE_HEIGHT);
-
-    scene.add(lights[0]);
-    scene.add(lights[1]);
-    scene.add(lights[2]);
-    scene.add(lights[3]);
-    scene.add(lights[4]);
-    scene.add(lights[5]);
+    initCamera();
+    initLights();
+    initWindow();
 
     fadingGroup = new THREE.Group();
     fadingGroup.name = 'fadingGroup';
@@ -105,8 +85,10 @@ function newInit() {
     scene.add(branchGroup);
 
     createTree(MAX_TREE_HEIGHT, SPREAD, Z_STEP);
+}
 
 
+function initWindow() {
     window.addEventListener('resize', function () {
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
@@ -136,6 +118,53 @@ function newInit() {
             cameraControls.update();
         }
     });
+}
+
+
+function initCamera() {
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000000);
+    camera.position.x = MAX_TREE_HEIGHT * .29;
+    camera.position.y = MAX_TREE_HEIGHT * .63;
+    camera.position.z = MAX_TREE_HEIGHT * .25;
+
+    camera.lookAt(0, MAX_TREE_HEIGHT / 2, MAX_TREE_HEIGHT);
+
+    renderer = new THREE.WebGLRenderer({antialias: true, preserveDrawingBuffer: true});
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0x000000, 1);
+    document.body.appendChild(renderer.domElement);
+
+    cameraControls = new OrbitControls(camera, renderer.domElement);
+    cameraControls.maxAzimuthAngle = Math.PI / 2;
+    cameraControls.maxPolarAngle = Math.PI / 2;
+    cameraControls.target = new THREE.Vector3(-2722, 32248, -4674);
+    cameraControls.update();
+
+}
+
+function initLights() {
+    var lights = [];
+    lights[0] = new THREE.PointLight(0xffffff, 1, 0);
+    lights[1] = new THREE.PointLight(0xffffff, 1, 0);
+    lights[2] = new THREE.PointLight(0xffffff, 1, 0);
+    lights[3] = new THREE.PointLight(0xffffff, 1, 0);
+    lights[4] = new THREE.PointLight(0xffffff, 1, 0);
+    lights[5] = new THREE.PointLight(0xffffff, 1, 0);
+
+    lights[0].position.set(0, MAX_TREE_HEIGHT, 0);
+    lights[1].position.set(MAX_TREE_HEIGHT, 0, 0);
+    lights[2].position.set(0, 0, -1 * MAX_TREE_HEIGHT);
+    lights[3].position.set(0, -1 * MAX_TREE_HEIGHT, 0);
+    lights[4].position.set(-1 * MAX_TREE_HEIGHT, 0, 0);
+    lights[5].position.set(0, 0, -1 * MAX_TREE_HEIGHT);
+
+    scene.add(lights[0]);
+    scene.add(lights[1]);
+    scene.add(lights[2]);
+    scene.add(lights[3]);
+    scene.add(lights[4]);
+    scene.add(lights[5]);
 }
 
 function shouldKeepGoing(branchNodesToSplit) {
